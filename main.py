483fa0f1ecc361b81f60956ff4ae141b3f95df88
zaan28 <marizaan.vanrooyen@msn.com>
2024-11-09 20:29:31 +0200

    add .gitignore and main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    ugh = sort_dict(chars_dict)
+    print(ugh)
+
+def sort_dict(chars_dict):
+    myKeys = list(chars_dict.keys())
+    myKeys.sort()
+    return(myKeys)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
